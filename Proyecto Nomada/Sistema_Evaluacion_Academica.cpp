3fb091acf447c19b6d3aa9241a417d73089d68cd
Jeanvidpol <jeanvidpol@outlook.es>
2026-03-29 21:26:17 -0500

    Adding initial commit

---
+++ Proyecto Nomada/Sistema_Evaluacion_Academica.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Desarrollar un programa que permita registrar 
+//las notas de estudiantes de un curso y 
+//calcular estadísticas académicas.
+
+int main() {
+
+    
+
+    return 0;
+}
